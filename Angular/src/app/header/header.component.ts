3db5e3066e0920c4671b84b27967d0db8d307f83
SantoshP <Padhysantosh42@gmail.com>
2020-01-04 00:22:43 +0400

    angular

---
+++ Angular/src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-header',
+    // template:`
+    // <h1>Header Component</h1>
+    // `,
+    // styles:[`h1{
+    //     background: black;
+    //     color: white;
+    //     padding: 20px;
+
+    // }`
+
+
+    // ]
+    templateUrl: './header.component.html',
+    styleUrls:['./header.component.css']
+})
+
+
+
+
+
+export class HeaderComponent{
+
+}
