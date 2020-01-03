37170854ad85edc13f1278b759a308b8d89b0bff
SantoshP <Padhysantosh42@gmail.com>
2020-01-04 00:25:01 +0400

    angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,50 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SectionComponent } from './section/section.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { JavaComponent } from './java/java.component';
-import { HtmlComponent } from './html/html.component';
-import { SpringComponent } from './spring/spring.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SectionComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactusComponent,
-    RegisterComponent,
-    LoginComponent,
-    PagenotfoundComponent,
-    JavaComponent,
-    HtmlComponent,
-    SpringComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-{path:'',component:HomeComponent},
-{path:'about',component:AboutComponent,children:[{path:'java',component:JavaComponent},{path:'html',component:HtmlComponent},
-  {path:'spring',component:SpringComponent}]},
-{path:'contactus',component:ContactusComponent},
-{path:'register',component:RegisterComponent},
-{path:'login',component:LoginComponent},
-{path:'**',component:PagenotfoundComponent}
- ])
-
-    
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
