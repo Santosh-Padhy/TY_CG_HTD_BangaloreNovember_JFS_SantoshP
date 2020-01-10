06a3e54d6e66f1ef328da32473ff153c04abd04e
SantoshP <Padhysantosh42@gmail.com>
2020-01-10 13:16:57 +0400

    fms jdbc

--- FMS/dao/AdminLoginDao.java
+++
@@ -1,5 +0,0 @@
-package com.capg.fms.dao;
-
-public interface AdminLoginDao {
-public boolean adminLogin(String username,String password);
-}
--- FMS/dao/ClientLoginDao.java
+++
@@ -1,5 +0,0 @@
-package com.capg.fms.dao;
-
-public interface ClientLoginDao {
-	public boolean clientLogin(String username,String password);
-}
